73f7d95bcd93b91bb0d1c85f1532901fa0247838
subhajit51193 <nnorth87@gmail.com>
2023-02-08 20:22:56 +0530

    customer exceptions created

---
+++ BookRecoomendationApp/src/main/java/com/app/spring/demo/exceptions/UserExceptions.java
@@ -0,0 +1,13 @@
+package com.app.spring.demo.exceptions;
+
+public class UserExceptions extends Exception{
+
+	public UserExceptions() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public UserExceptions(String message) {
+		// TODO Auto-generated constructor stub
+		super(message);
+	}
+}
